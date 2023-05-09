5ae4df561550abc80d5032b0c1081ba160fc3c8d
raj6939 <raj.v.patil108@gmail.com>
2023-05-09 11:32:51 +0530

    OG tag added directly in html

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,7 @@ export default {
   components: {
     certificate
   },
-  metaInfo() {
-    return {
-      meta: [
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { property: 'twitter:domain', content: 'master--og-check.netlify.app' },
-        { property: 'twitter:url', content: 'https://master--og-check.netlify.app/' },
-        { name: 'twitter:title', content: 'Og-test site' },
-        { name: 'twitter:description', content: 'Here is the tweet descp' },
-        { name: 'twitter:image', content: 'http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg' }
-      ]
-    };
-  }
+
 }
 </script>
 
